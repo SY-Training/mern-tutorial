d0b01a88f089d61093e47154fde4d73a091a1e2f
S-Y <sy-unimportant@outlook.com>
2022-05-25 19:40:11 +0100

    Configured Mongoose DB

--- server/index.js
+++ server/index.js
@@ -2,13 +2,22 @@
 // Just need to add the "type" key-value in package.json.
 
 // Also removed "test" from package.json and added start script for nodemon.
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import CONNECTION_URL from './Db_Access.js';
+
+
 
 const app = express(); //This initialises the node app.
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const PORT = process.env.PORT || 5000; // Use the process port or 5000.
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) // The {} is not needed. Just removes warnings.
+.then (() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))) // Error handling. 
+.catch((error) => console.log(error.message));
